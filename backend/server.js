e8257325585a412a85be60f72f6c16ddf45f0865
Braxon2 <dusanb80@gmail.com>
2025-02-03 11:54:34 +0100

    Added .env with port number

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+//routes
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello World!" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started on http://localhost:" + process.env.PORT);
 });
