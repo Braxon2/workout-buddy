c9abec5b1c20d59ea37b898041bff37ea88debe9
Braxon2 <dusanb80@gmail.com>
2025-02-04 11:24:51 +0100

    Made navbar and home components and connected front with the back

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import WorrkoutDetails from "../components/WorkoutDetails";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const respone = await fetch("/api/workouts");
+      const json = await respone.json();
+
+      if (respone.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorrkoutDetails key={workout.id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
